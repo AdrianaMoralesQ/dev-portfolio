aeb4d2e86ef8d317588ce08be810911668d173ba
Adriana Morales <amorales25@gmail.com>
2022-12-01 14:37:09 -0500

    formatted contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
-import { ContactForm } from "../Public/Styled Components";
+import { ContactForm, ContactWrapper } from "../Public/Styled Components";
 
 export const Contact = () => {
 	const form = useRef();
@@ -27,17 +27,32 @@ export const Contact = () => {
 			);
 	};
 	return (
-		<ContactForm>
-			<form ref={form} onSubmit={sendEmail}>
-				<label>Name</label>
-				<input type="text" name="user_name" />
-				<label>Email</label>
-				<input type="email" name="user_email" />
-				<label>Message</label>
-				<textarea name="message" />
-				<input type="submit" value="Send" />
-			</form>
-		</ContactForm>
+		<ContactWrapper>
+			<ContactForm>
+				<form ref={form} onSubmit={sendEmail}>
+					<label>Name:</label>
+					<input type="text" name="user_name" />
+					<label>Email:</label>
+					<input type="email" name="user_email" />
+					<label>Message:</label>
+					<textarea name="message" />
+					<input type="submit" value="Send" />
+				</form>
+			</ContactForm>
+			<div>
+				<p>Prefer to send me an email?</p>
+				<p>
+					Reach out to <b>amorales25@gmail.com.</b>
+				</p>
+				<p>
+					Or you can message me on{" "}
+					<a href="https://www.linkedin.com/in/adriana-morales-quiones/">
+						LinkedIn.
+					</a>
+				</p>
+				<p>I look forward to connecting soon 🫶</p>
+			</div>
+		</ContactWrapper>
 	);
 };
 export default Contact;
--- src/Public/Styled Components.js
+++ src/Public/Styled Components.js
@@ -131,10 +131,58 @@ export const SmallContentWrapper = styled.div`
 `;
 
 export const ContactForm = styled.div`
-	margin: 2rem;
+	width: 400px;
+	height: 600px;
+	form {
+		display: flex;
+		align-items: flex-start;
+		flex-direction: column;
+		width: 100%;
+		margin: 2rem;
+		width: 100%;
+		font-size: 19px;
+		color: #1d3557;
+		input {
+			width: 100%;
+			height: 35px;
+			padding: 7px;
+			outline: none;
+			border-radius: 5px;
+			border: 1px solid rgb(220, 220, 220);
+			&:focus {
+				border: 2px solid rgba(0, 206, 158, 1);
+			}
+		}
+		textarea {
+			max-width: 100%;
+			min-width: 100%;
+			width: 100%;
+			max-height: 200px;
+			min-height: 200px;
+			padding: 7px;
+			outline: none;
+			border-radius: 5px;
+			border: 1px solid rgb(220, 220, 220);
+			&:focus {
+				border: 2px solid rgba(0, 206, 158, 1);
+			}
+		}
+		label {
+			margin-top: 1rem;
+		}
+		input[type="submit"] {
+			margin-top: 2rem;
+			cursor: pointer;
+			background: #e63946;
+			color: white;
+			border: none;
+			font-size: 16px;
+		}
+	}
+`;
+export const ContactWrapper = styled.div`
 	display: flex;
-	align-items: flex-start;
-	flex-direction: column;
-	width: 100%;
-	font-size: 16px;
+	flex-direction: row;
+	justify-content: space-around;
+	font-size: 19px;
 `;
